b8b1a38620ea6c337de786f70a160a3d10de7f34
Andres Humberto Villamizar <andreshv2@gmail.com>
2020-02-11 20:48:02 -0500

    inclusion de fetch

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -4,7 +4,8 @@ import WeatherData from './WeatherData'
 
 import './style.css'
 import {
-    SUN
+    SUN,
+    CLOUDY
 } from '../../constants/weather'
 
 
@@ -17,17 +18,87 @@ const data={
 }
 
 
+
+/*const data2={
+    temperature:35,
+    weatherState:CLOUDY,
+    humidity:50,
+    wind:"7m/s"
+
+}*/
+
+
+const location="Buenos Aires,ar";
+const api_key="6831e58c07cd64a1c39225acabc3bca0";
+const url_base="http://api.openweathermap.org/data/2.5/weather";
+const api_weather= `${url_base}?q=${location}&appid=${api_key}`;
+
+
 class  WeatherLocation extends Component {
 
 
+    constructor(){
+        super();
+        this.state={
+          city:"Buenos aires",
+          data :data
+        }
+    }
+
+    getWeatherState= weather_data =>{
+        return SUN;     
+    }
+
+    getData = weather_data=>{
+        const {humidity, temp} =weather_data.main;
+        const {speed} =weather_data.wind;
+        const weatherState =this.getWeatherState(weather_data);
+        const data ={
+            humidity,
+            temperature:temp,
+            weatherState,
+            wind:`${speed} m/s`,
+
+        }
+        return data;
+    }
+
+
+    handleUpdateClick=()=>{
+        console.log("actualizado =>",api_weather);
+
+        fetch(api_weather).then(
+            resolve =>{
+               return resolve.json();
+            }
+        ).then(data=>{
+            console.log(data);
+            const newWeather = this.getData(data);
+            console.log(newWeather);
+
+            this.setState({data:newWeather});
+        });
+
+        /*this.setState({
+            city:"Barcelona",
+            data :data2
+        });*/
+
+    }
+
     render(){
+        const {city,data}=this.state;
         return (
         <div className="weatherLocationCont">
-            <Location city={"Pamplonita"}></Location>
+            <Location city={city}></Location>
             <WeatherData data={data}></WeatherData>
+            <button onClick={this.handleUpdateClick}>Actualizar</button>
         </div>
         )
     };
+
+
+
 
 }
 
